0296821dad24904d7d9e7a89b72a94781d096274
DmitriyVintov <dizdimm1@yandex.ru>
2023-11-19 13:33:02 +0300

    feat: реализация первого этапа

---
+++ ewm-stats-service/stats-server/src/main/java/ru.practicum/exception/ErrorHandler.java
@@ -0,0 +1,64 @@
+package ru.practicum.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+    @ResponseBody
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse onConstraintValidationException(
+            ConstraintViolationException e
+    ) {
+        final List<Violation> violations = e.getConstraintViolations().stream()
+                .map(
+                        violation -> new Violation(
+                                violation.getMessage()
+                        )
+                )
+                .collect(Collectors.toList());
+        log.error("Error 400. ConstraintValidation: {}", e.getMessage());
+        return new ValidationErrorResponse(violations);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ValidationErrorResponse onMethodArgumentNotValidException(
+            MethodArgumentNotValidException e
+    ) {
+        final List<Violation> violations = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        log.error("Error 400. MethodArgumentNotValid: {}", e.getMessage());
+        return new ValidationErrorResponse(violations);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        log.error("Error 400. Validation: {}", e.getMessage());
+        return new ErrorResponse(
+                e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        log.error("Error 500. UnknownError: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage()
+        );
+    }
+}
---
+++ ewm-stats-service/stats-server/src/main/java/ru.practicum/mapper/StatsMapper.java
@@ -0,0 +1,17 @@
+package ru.practicum.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.practicum.HitDto;
+import ru.practicum.model.Hit;
+import ru.practicum.utility.DateTimeFormat;
+
+@Mapper(componentModel = "spring")
+public interface StatsMapper {
+
+    @Mapping(target = "timestamp", source = "timestamp", dateFormat = DateTimeFormat.DATE_TIME_FORMAT)
+    HitDto toHitDto(Hit hit);
+
+    @Mapping(target = "timestamp", source = "timestamp", dateFormat = DateTimeFormat.DATE_TIME_FORMAT)
+    Hit toHit(HitDto hitDto);
+}
